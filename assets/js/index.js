e351601f484ad8ba9a16b166c0be439279109a7e
Matteo <matteo@agence-88.fr>
2023-10-31 19:59:10 +0100

    update html & css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,8 @@
 const $ = require('jquery');
 require('bootstrap');
+
+$('.deleteButton').on('click', function() {
+    var figureId = $(this).data('id');
+    var url = '/figure/' + figureId + '/delete';
+    $('#deleteConfirm').attr('href', url);
+});
